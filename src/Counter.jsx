882dc1977b7a3571697c9dc0ee901d8f2b3c53ad
Reydv <reyhanputraariutama@gmail.com>
2025-09-28 16:39:24 +0700

    initial commit

---
+++ src/Counter.jsx
@@ -0,0 +1,44 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+
+function Counter({ num, modifier }) {
+  return (
+    <>
+      <div className="h-screen w-screen flex justify-center items-center">
+        <div className="w-[400px] h-[200px] border-1 border-black">
+          <div className="w-full h-full flex flex-col">
+            <div className="w-full h-1/2 text-[50px] flex justify-center items-center">
+              {num}
+            </div>
+            <div className="w-full h-1/2 flex justify-center items-center gap-2">
+              <div className="w-full h-full flex justify-center items-center">
+                <button
+                  onClick={() => modifier(num - 1)}
+                  className="outline-2 outline-black"
+                >
+                  Kurang
+                </button>
+              </div>
+              <div className="w-full h-full flex justify-center items-center">
+                <button
+                  onClick={() => modifier(num + 1)}
+                  className="outline-2 outline-black"
+                >
+                  Tambah
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+function CounterInterface() {
+  const [count, setCount] = useState(0);
+
+  return <Counter num={count} modifier={setCount} />;
+}
+export default CounterInterface;
