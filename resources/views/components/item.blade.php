782e056d2879e950b13b6257ed5fee68ebfb01aa
Rafi K Yogananda <rafiyogananda9@gmail.com>
2025-09-28 19:22:24 +0700

    clear commit

--- resources/views/components/item.blade.php
+++
@@ -1,7 +0,0 @@
-@props(['name', 'stok', 'harga'])
-
-<div class="p-4 mb-4 border rounded shadow-sm flex justify-between items-center bg-white">
-    <p><strong>Name:</strong> {{ $name }}</p>
-    <p><strong>Stok:</strong> {{ $stok }}</p>
-    <p><strong>Harga:</strong> Rp {{ number_format($harga, 0, ',', '.') }}</p>
-</div>
--- resources/views/components/profile.blade.php
+++
@@ -1,7 +0,0 @@
-@props(['name', 'age', 'hobby'])
-
-<div class="p-4 mb-4 border rounded shadow-sm bg-white">
-    <p><strong>Name:</strong> {{ $name }}</p>
-    <p><strong>Age:</strong> {{ $age }} tahun</p>
-    <p><strong>Hobby:</strong> {{ $hobby }}</p>
-</div>
